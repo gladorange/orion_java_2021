bb1f3eb2de53846bccba892fd2cf22103a94f957
nvgladkov <ertzxcfgh473>
2021-05-21 14:05:01 +0300

    3 commit for lesson 6.

--- Gladkov/6/com/lesson6/Pair.java
+++ Gladkov/6/com/lesson6/Pair.java
@@ -3,9 +3,6 @@ package com.lesson6;
 import java.util.Objects;
 
 public class Pair <T1,T2> {
-
-    private static int ID = 0;
-    private int id;
     private T1 field1;
     private T2 field2;
 
@@ -13,7 +10,6 @@ public class Pair <T1,T2> {
     public Pair( T1 var1, T2 var2 ) {
         field1 = var1;
         field2 = var2;
-        this.id = ID++;
     }
 
 
@@ -31,8 +27,7 @@ public class Pair <T1,T2> {
         if ( this == o ) return true;
         if ( o == null || getClass() != o.getClass() ) return false;
         Pair<?, ?> pair = ( Pair<?, ?> ) o;
-        return id == pair.id &&
-                Objects.equals( field1, pair.field1 ) &&
+        return  Objects.equals( field1, pair.field1 ) &&
                 Objects.equals( field2, pair.field2 );
     }
 
@@ -40,7 +35,6 @@ public class Pair <T1,T2> {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + id;
         result = prime * result + ((field1 == null) ? 0 : field1.hashCode());
         result = prime * result + ((field2 == null) ? 0 : field2.hashCode());
         return result;
--- Gladkov/6/com/lesson6/Triple.java
+++ Gladkov/6/com/lesson6/Triple.java
@@ -3,8 +3,6 @@ package com.lesson6;
 import java.util.Objects;
 
 public class Triple<T1,T2,T3> {
-    private static int ID = 0;
-    private int id;
     private T1 field1;
     private T2 field2;
     private T3 field3;
@@ -14,7 +12,6 @@ public class Triple<T1,T2,T3> {
         field1 = var1;
         field2 = var2;
         field3 = var3;
-        this.id = ID++;
     }
 
 
@@ -36,8 +33,7 @@ public class Triple<T1,T2,T3> {
         if ( this == o ) return true;
         if ( o == null || getClass() != o.getClass() ) return false;
         Triple<?, ?, ?> triple = ( Triple<?, ?, ?> ) o;
-        return id == triple.id &&
-                Objects.equals( field1, triple.field1 ) &&
+        return  Objects.equals( field1, triple.field1 ) &&
                 Objects.equals( field2, triple.field2 ) &&
                 Objects.equals( field3, triple.field3 );
     }
@@ -46,7 +42,6 @@ public class Triple<T1,T2,T3> {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + id;
         result = prime * result + ((field1 == null) ? 0 : field1.hashCode());
         result = prime * result + ((field2 == null) ? 0 : field2.hashCode());
         result = prime * result + ((field3 == null) ? 0 : field3.hashCode());
