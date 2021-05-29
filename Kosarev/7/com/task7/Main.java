c291d1c0d8c7ce42afcd28e25f82fc396f9ce9cf
Roman Kosarev <tempbox0@bk.ru>
2021-05-29 10:36:27 +0300

    Lesson 7 - homework

---
+++ Kosarev/7/com/task7/Main.java
@@ -0,0 +1,16 @@
+package com.task7;
+
+
+import com.task7.Boxes.NumberBox;
+import com.task7.Boxes.StringBoxUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("NumberBox task:");
+        NumberBox.run();
+        System.out.println("--------------------------------------------------");
+        System.out.println("StringBoxUtils task:");
+        StringBoxUtils.run();
+        System.out.println("--------------------------------------------------");
+    }
+}
